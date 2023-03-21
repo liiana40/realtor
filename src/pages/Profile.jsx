89ad07e5a354d21c02207ce329d07df1f1fea55e
liiana40 <l_ignat@yahoo.com>
2023-03-21 16:02:53 -0400

    add delete and edit functionality to the my listing section

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,145 +1,70 @@
-import { getAuth, updateProfile } from 'firebase/auth';
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from "react-toastify";
-import {
-  collection,
-  deleteDoc,
-  doc,
-  getDocs,
-  orderBy,
-  query,
-  updateDoc,
-  where,
-} from "firebase/firestore";
-import { db } from "../firebase";
-import {FcHome} from "react-icons/fc";
-import { useEffect } from "react";
-import listingItem from '../components/listingItem';
-
-export default function Profile() {
-  const auth = getAuth()
-  const navigate = useNavigate();
-  const [changeDetail, setChangeDetail] = useState(false);
-  const [listings, setListings ] = useState(null);
-  const [loading, setLoading ] = useState(true);
-  const [formData, setFormData] = useState({
-    name: auth.currentUser.displayName,
-    email: auth.currentUser.email
-  });
-  const {name, email} = formData
-  function onLogout(){
-    auth.signOut()
-    navigate("/")
-  }
-  function onChange(e) {
-    setFormData((prevState)=> ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  }
-  async function onSubmit() {
-    try {
-      if (auth.currentUser.displayName !== name) {
-        //update display name in firebase auth
-        await updateProfile(auth.currentUser, {
-          displayName: name,
-        });
-        //update name in the firestore 
-        const docRef = doc(db, "users", auth.currentUser.uid)
-        await updateDoc(docRef, {
-          name, 
-        });
-      }
-      toast.success('Profile details updated')
-    } catch (error) {
-      toast.error("Could not update the profile detail")
-    }
-  }
-  useEffect(()=>{
-    async function fetchUserListing(){
-      
-      const listingRef = collection(db, "listings");
-      const q = query(listingRef, where("userRef", "==", auth.currentUser.uid),
-      orderBy("timestamp", "desc"));
-      const querySnap = await getDocs(q);
-      let listings = [];
-      querySnap.forEach((doc)=> {
-        return listings.push({
-          id: doc.id,
-          data: doc.data(),
-        });
-      });
-      setListings(listings)
-      setLoading(false);
-    }
-    fetchUserListing();
-  },[auth.currentUser.uid]);
+import Moment from "react-moment";
+import { Link } from "react-router-dom";
+import { MdLocationOn } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
+export default function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
-    <>
-      <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
-        <h1 className='text-3xl text-center mt-6 font-bold'>My Profile</h1>
-        <div className='w-full md:w-[50%] mt-6 px-3'>
-          <form>
-            {/* Name Input*/}
-            <input type="text" id='name' value={name} disabled = {!changeDetail}
-              onChange={onChange}
-              className={`mb-6 w-full px-4 py-2 text-xl text-grey-700 bg-white border
-              border-grey-300 rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`}
-            />
-            {/* Email Input*/}
-            <input type="email" id='email' value={email} disabled
-              className='mb-6 w-full px-4 py-2 text-xl text-grey-700 bg-white border
-              border-grey-300 rounded transition ease-in-out'
-            />
-            <div className='flex justify-between whitespace-nowrap
-            text-sm sm:text-lg mb-6'>
-              <p className='flex items-center '>Do you want to change your name?
-                <span 
-                onClick={()=> {
-                  changeDetail && onSubmit()
-                  setChangeDetail((prevState)=> !prevState)
-                }}
-                className='text-red-600 hover: text-red-700
-                transition ease-in-out duration-200 ml-1 cursor-pointer'>
-                  {changeDetail ? "Apply change" : "Edit"}
-                </span>
+    <li className="relative bg-white flex flex-col justify-between items-center shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
+      <Link className="contents" to={`/category/${listing.type}/${id}`}>
+        <img
+          className="h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in"
+          loading="lazy"
+          src={listing.imgUrls[0]}
+        />
+        <Moment
+          className="absolute top-2 left-2 bg-[#3377cc] text-white uppercase text-xs font-semibold rounded-md px-2 py-1 shadow-lg"
+          fromNow
+        >
+          {listing.timestamp?.toDate()}
+        </Moment>
+        <div className="w-full p-[10px]">
+          <div className="flex items-center space-x-1">
+            <MdLocationOn className="h-4 w-4 text-green-600" />
+            <p className="font-semibold text-sm mb-[2px] text-gray-600 truncate">
+              {listing.address}
+            </p>
+          </div>
+          <p className="font-semibold m-0 text-xl truncate">{listing.name}</p>
+          <p className="text-[#457b9d] mt-2 font-semibold">
+            $
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" && " / month"}
+          </p>
+          <div className="flex items-center mt-[10px] space-x-3">
+            <div className="flex items-center space-x-1">
+              <p className="font-bold text-xs">
+                {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
               </p>
-              <p onClick={onLogout} className='text-blue-600 hover:text-blue-800
-              transition duration-200 ease-in-out cursor-pointer'>Sign out</p>
             </div>
-          </form>
-          <button 
-          type='submit' className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded 
-          shodow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>
-            <Link to = "/create-listing" className='flex justify-center items-center'>
-              <FcHome className='mr-2 text-3xl bg-red-200 rounded-full p-1 border-2'/>
-              Sell or rent your home
-            </Link>
-            
-          </button>
+            <div className="flex items-center space-x-1">
+              <p className="font-bold text-xs">
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} Baths`
+                  : "1 Bath"}
+              </p>
+            </div>
+          </div>
         </div>
-      </section>
-      <div className='max-w-6xl px-3 mt-6 mx-auto'>
-        {!loading && listings.length > 0 && (
-          <>
-            <h2 className='text-2xl text-center font-semibold mb-6 mt-6'>
-              My Listings
-            </h2>
-            <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 
-            2xl:grid-cols-5 mt-6 mb-6' >
-              {listings.map((listing)=> (
-                <listingItem
-                  key={listing.id} 
-                  id = {listing.id} 
-                  listing= {listing.data}
-                />
-              ))}
-            </ul> 
-          </>
-        )}
-      </div>
-    </>
-  )
+      </Link>
+      {onDelete && (
+        <FaTrash
+          className="absolute bottom-2 right-2 h-[14px] cursor-pointer text-red-500"
+          onClick={() => onDelete(listing.id)}
+        />
+      )}
+      {onEdit && (
+        <MdEdit
+          className="absolute bottom-2 right-7 h-4 cursor-pointer "
+          onClick={() => onEdit(listing.id)}
+        />
+      )}
+    </li>
+  );
 }
- 
